d7bb2a53a7de465f3689d482a48b56f7bef3d1b8
Rayhan <rgozali48@gmail.com>
2026-07-23 10:47:10 +0700

    persiapan backend untuk produksion deploy

--- server/src/app.js
+++ server/src/app.js
@@ -14,10 +14,15 @@ import userRoutes from "./modules/user/user.routes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      process.env.CLIENT_URL,
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
